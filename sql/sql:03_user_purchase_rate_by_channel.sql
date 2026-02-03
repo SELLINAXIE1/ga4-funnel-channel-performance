abf1a2c64848e7913f23614adeba73f0b7c881b6
SELLINAXIE1 <157312058+SELLINAXIE1@users.noreply.github.com>
2026-02-02 23:55:37 -0500

    Add files via upload

---
+++ sql/sql:03_user_purchase_rate_by_channel.sql
@@ -0,0 +1,13 @@
+source,medium,sessions_users,purchasers_users,user_purchase_rate
+google,organic,102530,1229,0.011986735589583537
+(direct),(none),75025,1054,0.014048650449850051
+(data deleted),(data deleted),16969,680,0.040073074429842655
+shop.googlemerchandisestore.com,referral,25350,568,0.022406311637080869
+<Other>,<Other>,50776,499,0.009827477548448085
+<Other>,referral,32362,468,0.014461405351955997
+google,cpc,15449,152,0.0098388245193863685
+<Other>,organic,10012,97,0.00968837395125849
+<Other>,(data deleted),372,9,0.024193548387096774
+(data deleted),referral,2,0,0.0
+<Other>,cpc,1,0,0.0
+google,<Other>,1,0,0.0
---
+++ sql/sql:04_session_funnel_by_channel.sql
@@ -0,0 +1,13 @@
+source,medium,sessions,sessions_view_item,sessions_add_to_cart,sessions_begin_checkout,sessions_purchase,session_purchase_rate
+google,organic,111488,23241,4261,3073,1209,0.010844216417910448
+(direct),(none),82362,17284,3439,2437,1046,0.012700031567956096
+(data deleted),(data deleted),21087,5268,1387,1135,673,0.031915398112581213
+shop.googlemerchandisestore.com,referral,28044,6371,1420,1087,555,0.019790329482242189
+<Other>,<Other>,51775,10475,1963,1357,495,0.0095605987445678414
+<Other>,referral,34110,7243,1429,1037,460,0.013485781295807681
+google,cpc,15534,3136,556,377,146,0.0093987382515771847
+<Other>,organic,10078,2044,387,237,93,0.0092280214328239723
+<Other>,(data deleted),375,90,20,18,9,0.024
+(data deleted),referral,2,1,0,1,0,0.0
+<Other>,cpc,1,1,1,0,0,0.0
+google,<Other>,1,0,0,0,0,0.0
